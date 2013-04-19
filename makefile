897c366173d3c2ab9f9cf17f7f89957529d362af
Bora Banjanin <borabanjanin@yahoo.com>
2013-04-19 13:23:58 -0700

    mass update

--- makefile
+++ makefile
@@ -1,2 +1,2 @@
 all: rs232.c main.c 
-	gcc main.c rs232.c -o3 -o emergency
+	gcc main.c rs232.c -o3 -o emergency -lm
--- processdata.c
+++ processdata.c
@@ -1,9 +1,18 @@
 #ifndef PROCESSDATA
 #define PROCESSDATA
-
+#include <math.h>
 
 int ProcessData(){
-	printf("success\n");
+	double xaccel_squar = (double) Samples[samplenumber].xaccel;
+	double yaccel_squar = (double) Samples[samplenumber].yaccel;
+	double zaccel_squar = (double) Samples[samplenumber].zaccel;
+	double accel;
+
+	xaccel_squar = pow(xaccel_squar,2.0);
+	yaccel_squar = pow(yaccel_squar,2.0);
+	zaccel_squar = pow(zaccel_squar,2.0);
+	accel_array[samplenumber] = sqrt(xaccel_squar + yaccel_squar + zaccel_squar);
+	fprintf(logfile,"Acceleration: %f\n",accel_array[samplenumber]);
 	return 0;
 }
 
