278f6cf1f5a6167be9d00d7a477516067aff3556
Bora Banjanin <borabanjanin@yahoo.com>
2013-04-24 12:15:11 -0700

    complete overhaul of data processing

--- main.c
+++ main.c
@@ -55,123 +55,29 @@ int ProcessInput(){
 	signed int sign_exten_mask = 0x00008000;
 	for(i=0; i < buffersize; i++){
 		char inputbyte =  buf[i];
-		if(typeinput){
-			printf("%i ",inputbyte);
-		}else{
-			printf("\n");
-		}	
-		switch(typeinput){
-			case 0:
+
+			if(typeinput == 0){
 				if(inputbyte == 85){
 					typeinput++;
-//					fprintf(logfile, "%d ",samplenumber);
-//					printf("%d ",samplenumber);
-					Samples[samplenumber].n = samplenumber;
-				}
-				break;
-
-			case 1:
-				measurement = inputbyte & 0x000000FF;
-				typeinput++;
-				break;
-
-			case 2:
-				measurement |= (inputbyte & 0x000000FF) << 8;
-				if(sign_exten_mask & measurement){
-					measurement |= 0xFFFF0000;
-				}
-				Samples[samplenumber].xaccel = measurement;
-//				fprintf(logfile,"%d ",Samples[samplenumber].xaccel);
-//				printf("%d ",Samples[samplenumber].xaccel);
-				typeinput++;
-				break;
-
-			case 3:
-				measurement = inputbyte & 0x000000FF;
-				typeinput++;
-				break;
-
-			case 4:
-				measurement |= (inputbyte & 0x000000FF) << 8;
-				if(sign_exten_mask & measurement){
-					measurement |= 0xFFFF0000;
-				}
-				Samples[samplenumber].yaccel = measurement;
-//				fprintf(logfile,"%d ",Samples[samplenumber].yaccel);
-//				printf("%d ",Samples[samplenumber].yaccel);
-				typeinput++;
-				break;
-
-			case 5:
-				measurement = inputbyte & 0x000000FF;
-				typeinput++;
-				break;
-
-			case 6:
-				measurement |= (inputbyte & 0x000000FF) << 8;
-				if(sign_exten_mask & measurement){
-					measurement |= 0xFFFF0000;
-				}
-				Samples[samplenumber].zaccel = measurement;
-//				fprintf(logfile,"%d ",Samples[samplenumber].zaccel);
-//				printf("%d ",Samples[samplenumber].zaccel);
-				typeinput++;
-				break;
-
-			case 7:
-				measurement = inputbyte & 0x000000FF;
-				typeinput++;
-				break;
-
-			case 8:
-				measurement |= (inputbyte & 0x000000FF) << 8;
-				if(sign_exten_mask & measurement){
-					measurement |= 0xFFFF0000;
-				}
-				Samples[samplenumber].xrot = measurement;
-//				fprintf(logfile,"%d ",Samples[samplenumber].xrot);
-//				printf("%d ",Samples[samplenumber].xrot);
-				typeinput++;
-				break;
-
-			case 9:
-				measurement = inputbyte & 0x000000FF;
-				typeinput++;
-				break;
-
-			case 10:
-				measurement |= (inputbyte & 0x000000FF) << 8;
-				if(sign_exten_mask & measurement){
-					measurement |= 0xFFFF0000;
-				}
-				Samples[samplenumber].yrot = measurement;
-//				fprintf(logfile,"%d ",Samples[samplenumber].yrot);
-//				printf("%d ",Samples[samplenumber].yrot);
-				typeinput++;
-				break;
-
-			case 11:
-				measurement = inputbyte & 0x000000FF;
-				typeinput++;
-				break;
-
-			case 12:
-				measurement |= (inputbyte & 0x000000FF) << 8;
-				if(sign_exten_mask & measurement){
-					measurement |= 0xFFFF0000;
-				}
-				Samples[samplenumber].zrot = measurement;
-//				fprintf(logfile,"%d ",Samples[samplenumber].zrot);
-//				printf("%d ",Samples[samplenumber].zrot);
-				typeinput = 0;
-				ProcessData();
-				if(samplenumber < 999){
 					samplenumber++;
-				}else{
-					samplenumber = 0;
 				}
-				break;
+			}else if(typeinput == 1 || typeinput == 3 || typeinput == 5 || typeinput == 7 || typeinput == 9 || typeinput == 11){
+				measurement = inputbyte & 0x000000FF;
+				typeinput++;
+			}else if(typeinput == 2 || typeinput == 4 || typeinput == 6 || typeinput == 8 || typeinput == 10 || typeinput == 12){
+				measurement |= (inputbyte & 0x000000FF) << 8;
+				if(sign_exten_mask & measurement){
+					measurement |= 0xFFFF0000;
+				}
+				typeinput++;
+			}			
+				
+		if(samplenumber < 999){
+				samplenumber++;
+		}else{
+				samplenumber = 0;
 		}
+
 	}
 
 	return 0;
