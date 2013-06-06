c4e2f88c1f0c93b789bcbeade40a478233b031c0
Bora Banjanin <borabanjanin@yahoo.com>
2013-06-06 01:17:07 +0000

    working version

--- processdata.c
+++ processdata.c
@@ -189,7 +189,7 @@ int StandingCalibration(short sensor_id, SensorInfo* sensor){
 		cali_chest.x_standing_angle = sensor->xangle_comp;
 		cali_chest.y_standing_angle = sensor->yangle_comp;
 		cali_chest.z_standing_angle = sensor->zangle_comp;
-		cali_chest.sitting_fill = TRUE;
+		cali_chest.standing_fill = TRUE;
 	}else if(sensor_id == THIGH){
 		cali_thigh.x_standing_angle = sensor->xangle_comp;
 		cali_thigh.y_standing_angle = sensor->yangle_comp;
@@ -341,17 +341,19 @@ int FallDetection(short sensor_id, SensorInfo* sensor, Sample* point){
 		if(chest_info.xangle_comp  < cali_chest.x_standing_angle + 30 && chest_info.xangle_comp >  cali_chest.x_standing_angle - 30){
 			if(chest_info.yangle_comp  < cali_chest.y_standing_angle + 30 && chest_info.yangle_comp >  cali_chest.y_standing_angle - 30){
 				if(chest_info.zangle_comp  < cali_chest.z_standing_angle + 30 && chest_info.zangle_comp >  cali_chest.z_standing_angle - 30){
-				//	if(thigh_info.xangle_comp  < cali_chest.x_standing_angle * 1.3 && thigh_info.xangle_comp >  cali_chest.x_standing_angle * 0.7){
-				//		if(thigh_info.yangle_comp  < cali_chest.y_standing_angle * 1.3 && thigh_info.yangle_comp >  cali_chest.y_standing_angle * 0.7){
-				//			if(thigh_info.zangle_comp  < cali_chest.z_standing_angle * 1.3 && thigh_info.zangle_comp >  cali_chest.z_standing_angle * 0.7){
+					if(thigh_info.xangle_comp  < cali_chest.x_standing_angle + 30 && thigh_info.xangle_comp >  cali_chest.x_standing_angle - 30){
+						if(thigh_info.yangle_comp  < cali_chest.y_standing_angle + 30 && thigh_info.yangle_comp >  cali_chest.y_standing_angle - 30){
+							if(thigh_info.zangle_comp  < cali_chest.z_standing_angle + 30 && thigh_info.zangle_comp >  cali_chest.z_standing_angle - 30){
 								printf("FALL DETECTION AVERTED STANDING\n");
 								fall_detected = 0;
-				//			}
-				//		}
-			//		}
+							}
+						}
+					}
 				}
 			}
 		}
+	}else{
+		printf("Not Standing Fill");
 	}
 
 	if(fall_detected == 4){
